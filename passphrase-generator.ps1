a05a17a463e595013d4d2c6b4aa7edf76d679f54
kevinxtong <122850062+kevinxtong@users.noreply.github.com>
2023-01-19 21:25:21 -0500

    Update passphrase-generator.ps1

--- passphrase-generator.ps1
+++ passphrase-generator.ps1
@@ -24,24 +24,69 @@ param(
         }
     }
     $passphrase = $randomWords -join "-"
-    if($passphrase.Length -lt 15){
+    if($passphrase.Length -lt 16){
         $passphrase = New-Passphrase
     }
     $passphrase
 }
 
-function Show-PassphraseForm
+function Show-PassPhraseForm
 {
-    
-    $PopupTitle = "New-Passphrase"
-    $PopupMessage = "Press OK to reroll or CTRL + C to copy."
-    $PopUpTextField = (New-Passphrase)
-    Add-Type -AssemblyName Microsoft.VisualBasic
-    $input = [Microsoft.VisualBasic.Interaction]::InputBox($PopupMessage, $PopupTitle, $PopUpTextField)
-    if($input -ne ""){
-       Show-PassphraseForm
-    }
+Add-Type -AssemblyName System.Windows.Forms
+$form = New-Object System.Windows.Forms.Form
+$form.TopMost = $true
+$form.Text = "PassphraseGen"
+$form.Width = 350
+$form.Height = 150
+$form.StartPosition = "CenterScreen"
+
+# Passphrase input box
+$passphraseBox1 = New-Object System.Windows.Forms.TextBox
+$passphraseBox1.Width = 300
+$passphraseBox1.Height = 20
+$passphraseBox1.Font = New-Object System.Drawing.Font("Consolas", 13.5)
+$passphraseBox1.Text = (New-PassPhrase)
+$passphraseBox1.Dock = [System.Windows.Forms.DockStyle]::Fill
+
+# Copy to clipboard button for passphraseBox1
+$copyButton1 = New-Object System.Windows.Forms.Button
+$copyButton1.Width = 75
+$copyButton1.Height = 25
+$copyButton1.Text = "Copy"
+$copyButton1.Dock = [System.Windows.Forms.DockStyle]::Bottom
+$copyButton1.Add_Click({
+    [System.Windows.Forms.Clipboard]::SetText($passphraseBox1.Text)
+})
+
+# Regenerate passphrase button
+$generateButton = New-Object System.Windows.Forms.Button
+$generateButton.Width = 75
+$generateButton.Height = 25
+$generateButton.Text = "Regenerate"
+$generateButton.Dock = [System.Windows.Forms.DockStyle]::Bottom
+$generateButton.Add_Click({
+    $passphraseBox1.Text = (New-PassPhrase)
+})
+
+$panel = New-Object System.Windows.Forms.Panel
+$panel.Padding = New-Object System.Windows.Forms.Padding(10, 10, 10, 10)
+$panel.Dock = [System.Windows.Forms.DockStyle]::Fill
+$form.Controls.Add($panel)
+
+$passphraseBox1.Dock = [System.Windows.Forms.DockStyle]::Fill
+$panel.Controls.Add($passphraseBox1)
+
+$copyButton1.Dock = [System.Windows.Forms.DockStyle]::Bottom
+$panel.Controls.Add($copyButton1)
+
+$generateButton.Dock = [System.Windows.Forms.DockStyle]::Bottom
+$panel.Controls.Add($generateButton)
+
+
+
+$form.ShowDialog()
+
 }
 
 
-Show-PassphraseForm
+Show-PassPhraseForm
